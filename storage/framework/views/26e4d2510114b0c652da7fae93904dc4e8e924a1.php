cf908da3c098e9a0a59fee822a1553e4d7205007
Mansha Qarib <mansha.qarib777@gmail.com>
2021-04-22 21:38:33 +0500

    .gitignore is now working

--- storage/framework/views/26e4d2510114b0c652da7fae93904dc4e8e924a1.php
+++
@@ -1,9 +0,0 @@
-
-<ul class="product__review">
-    <?php for($i=0; $i<$rate; $i++): ?>
-        <li class="product__review--fill"><i class="icon-star"></i></li>
-    <?php endfor; ?>
-    <?php for($i=0; $i<5-$rate; $i++): ?>
-        <li class="product__review--blank"><i class="icon-star"></i></li>    
-    <?php endfor; ?>    
-</ul><?php /**PATH /var/www/html/food/resources/views/frontend/layouts/components/product_review.blade.php ENDPATH**/ ?>
