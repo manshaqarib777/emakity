669efa79e3ced9f69148516eddb3aabf2b2f328c
Mansha Qarib <mansha.qarib777@gmail.com>
2021-06-30 09:54:10 +0500

    fixes

--- resources/views/settings/areas/fields.blade.php
+++ resources/views/settings/areas/fields.blade.php
@@ -43,7 +43,7 @@
                 placeholder: "Select state",
             });
 
-            $('.select-country').trigger('change');
+
 
             $('#change-country').change(function() {
                 var id = $(this).val();
@@ -61,6 +61,7 @@
 
                 });
             });
+            $('.select-country').trigger('change');
         });
     </script>
 @endpush
