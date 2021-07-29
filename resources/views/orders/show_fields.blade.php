670763d2a77f9f3b08655627f38e5671a50fdf66
mansha.qarib777@gmail.com <mansha.qarib777@gmail.com>
2021-07-29 23:04:22 +0500

    fixes

--- resources/views/orders/show_fields.blade.php
+++ resources/views/orders/show_fields.blade.php
@@ -99,7 +99,7 @@
     </div>
     {!! Form::label('delivery_time', trans('lang.delivery_time'), ['class' => 'col-4 control-label']) !!}
     <div class="col-8">
-        <p>{!! $order->deliveryTime->name !!}</p>
+        <p>{!! @$order->deliveryTime->name !!}</p>
     </div>
 
 </div>
